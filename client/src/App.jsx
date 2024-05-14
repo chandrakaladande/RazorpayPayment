4bdf4b0bb8de9e65677e79e98e3c981f4adb773b
chandrakala918 <chandrakaladande918@gmail.com>
2024-05-14 12:41:23 +0530

    Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,53 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const paymentHandler = async () => {
+  const [amount, setAmount] = useState(500); // Default initial amount
 
-    const amount = 500;
+  const handleAmountChange = (e) => {
+    setAmount(parseInt(e.target.value, 10));
+  };
+
+  const paymentHandler = async () => {
     const currency = 'INR';
     const receiptId = '1234567890';
-    const response = await fetch("http://localhost:5000/order" ,{
+    const response = await fetch("http://localhost:5000/order", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        amount ,
+        amount,
         currency,
-        receipt : receiptId
+        receipt: receiptId
       })
-    })
+    });
 
     const order = await response.json();
-    console.log("order",order);
+    console.log("order", order);
 
-
-    var option = {
-      key:"",
+    const option = {
+      key: "rzp_test_pfAIujHWed8MgZ", // Your Razorpay key
       amount,
       currency,
-      name:"chandrakala",
+      name: "chandrakala",
       description: "Test Transaction",
-      image:"https://i.ibb.co/5Y3m33n/test.png",
-      order_id:order.id,
-      handler: async function(response) {
-
-        const body = {...response,}
+      image: "https://i.ibb.co/5Y3m33n/test.png",
+      order_id: order.id,
+      handler: async function (response) {
+        const body = { ...response };
 
         const validateResponse = await fetch('http://localhost:5000/validate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-
-        })
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        });
 
         const jsonResponse = await validateResponse.json();
 
         console.log('jsonResponse', jsonResponse);
-
       },
       prefill: {
         name: "chandrakala",
@@ -61,10 +60,10 @@ function App() {
       theme: {
         color: "#3399cc",
       },
-    }
+    };
 
     var rzp1 = new Razorpay(option);
-    rzp1.on("payment.failed", function(response) {
+    rzp1.on("payment.failed", function (response) {
       alert(response.error.code);
       alert(response.error.description);
       alert(response.error.source);
@@ -72,21 +71,28 @@ function App() {
       alert(response.error.reason);
       alert(response.error.metadata.order_id);
       alert(response.error.metadata.payment_id);
-    })
+    });
 
     rzp1.open();
-
-}
-
+  };
 
   return (
-  <>
-  <div className='product'>
-    <h1>Razorpay Payment Gateway</h1>
-    <button className='button' onClick={paymentHandler}>Pay Now</button>
-  </div>
-  </>
-  )
+    <>
+      <div className='product'>
+        <h1>Razorpay Payment Gateway</h1>
+        <input
+          type="number"
+          value={amount}
+          onChange={handleAmountChange}
+          min={1}
+          step={1}
+        />
+        <button className='button' onClick={paymentHandler}>
+          Pay Now
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
